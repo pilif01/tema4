ffc781fcb6f9683dde6512f0a7d90b67ee55e3de
pilif01 <129959994+pilif01@users.noreply.github.com>
2024-12-16 10:36:32 +0200

    Update ResourceManager.cpp

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -1,6 +1,5 @@
 #include "ResourceManager.h"
 
-// Constructor: inițializează resursa
 ResourceManager::ResourceManager(const std::string& resourceName)
     : resourceName(resourceName) {
     std::cout << "Resource " << resourceName << " acquired.\n";
@@ -11,13 +10,11 @@ ResourceManager::~ResourceManager() {
     std::cout << "Resource " << resourceName << " released.\n";
 }
 
-// Move constructor: transferă posesia resursei
 ResourceManager::ResourceManager(ResourceManager&& other) noexcept
     : resourceName(std::move(other.resourceName)) {
     std::cout << "Resource " << resourceName << " moved.\n";
 }
 
-// Move assignment operator: transferă posesia resursei
 ResourceManager& ResourceManager::operator=(ResourceManager&& other) noexcept {
     if (this != &other) {
         resourceName = std::move(other.resourceName);
@@ -26,7 +23,6 @@ ResourceManager& ResourceManager::operator=(ResourceManager&& other) noexcept {
     return *this;
 }
 
-// Utilizare resursă
 void ResourceManager::useResource() const {
     if (!resourceName.empty()) {
         std::cout << "Using resource: " << resourceName << "\n";
